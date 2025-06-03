70ae87bb4abb2fe2f7ae54654497f3e48c2c92ef
M.Fadli Ramadhan <fadli0127mfr@gmail.com>
2025-06-03 13:32:49 +0800

    Menampilkan data pada array

--- hello.cpp
+++ hello.cpp
@@ -3,16 +3,19 @@
 using namespace std;
 
 int main() {
-    // Inisialisasi array string
     string nama[5];
 
-    // Memasukkan 5 nama
     nama[0] = "Fadli";
     nama[1] = "Anshari";
     nama[2] = "Pepen";
     nama[3] = "Rayhan";
     nama[4] = "Ijan";
 
+    // Menampilkan data nama
+    cout << "Daftar Nama:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << i + 1 << ". " << nama[i] << endl;
+    }
+
     return 0;
 }
-
